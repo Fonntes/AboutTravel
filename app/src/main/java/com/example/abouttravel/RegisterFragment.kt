f9afa8269edebe57bd0d5cc43d2d26070d696397
Gonçalo Fonte <goncalofonte82@gmail.com>
2024-05-16 16:01:04 +0100

    finish basic on login and register design

--- app/src/main/java/com/example/abouttravel/RegisterFragment.kt
+++ app/src/main/java/com/example/abouttravel/RegisterFragment.kt
@@ -5,7 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 
 class RegisterFragment : Fragment() {
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_register, container, false)
+
+        /*val button = view.findViewById<Button>(R.id.button_register_login)
+        button.setOnClickListener{
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment2)
+        }*/
+
+        return view
+    }
 }
