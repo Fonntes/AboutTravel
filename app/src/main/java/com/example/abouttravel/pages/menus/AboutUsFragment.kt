b575118c62eb1460a89471dfccf4a0981fa96127
Gonçalo Fonte <goncalofonte82@gmail.com>
2024-06-28 04:22:30 +0100

    About us Created

--- app/src/main/java/com/example/abouttravel/pages/menus/AboutUsFragment.kt
+++ app/src/main/java/com/example/abouttravel/pages/menus/AboutUsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.abouttravel.pages.menus
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,19 +11,20 @@ import androidx.navigation.fragment.findNavController
 import com.example.abouttravel.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class DefinitionFragment : Fragment() {
+class AboutUsFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        val view = inflater.inflate(R.layout.fragment_definition, container, false)
+        val view = inflater.inflate(R.layout.fragment_about_us, container, false)
 
         val bottomNavigationView = view.findViewById<BottomNavigationView>(R.id.navbar)
 
-        val backButton = view.findViewById<Button>(R.id.Sobrenos)
+        val backButton = view.findViewById<Button>(R.id.back)
         backButton.setOnClickListener {
-            findNavController().navigate(R.id.action_definitionFragment_to_aboutUsFragment)
+            findNavController().navigate(R.id.action_aboutUsFragment_to_definitionFragment)
         }
 
 
@@ -50,6 +52,4 @@ class DefinitionFragment : Fragment() {
 
         return view
     }
-
-
 }
